d06078c7f4fc83c2c004262ca4b2d57469909ba8
Ben <bjbraham@gmail.com>
2022-08-27 23:01:54 +1000

    Add a lot of stuff oops im sorry

--- canva_app/my-app/src/index.js
+++ canva_app/my-app/src/index.js
@@ -1,10 +1,68 @@
 const { imageHelpers } = window.canva;
 const canva = window.canva.init();
 
+class ControlAttribute {
+  constructor(label, type, values) {
+    this.label = label;
+    this.type = type;
+    this.values = values;
+    this.current = "";
+    switch (this.type) {
+      case "slider":
+        this.control = canva.create("slider", {
+          id: label,
+          label: label,
+          value: 0,
+          min: 0,
+          max: values.length - 1,
+          step: 1,
+        });
+        break;
+      case "checkbox":
+        this.control = canva.create("checkbox", {
+          id: label,
+          label: label,
+          disabled: false,
+          checked: false,
+        });
+        break;
+      case "select":
+        this.control = canva.create("select", {
+          id: label,
+          value: values[0],
+          options: values
+        });
+      default:
+        console.log("Invalid type: ", type)
+    }
+  }
+};
+
+const advancedControlAttributes = {
+  "Van-Go": new ControlAttribute("Van-Go", "slider", [
+    "", "With a Van", "In impressionist style",
+    "In post-impressionist style", "In the style of Van Gogh"]
+  ),
+  "Anime Style": new ControlAttribute("Anime Style", "checkbox", ["", "In the style of anime"]),
+  "Apocalypse": new ControlAttribute("Apocalypse", "checkbox", ["", "Everything is on fire and there are demons in my butthoole"]),
+  "Art Style": new ControlAttribute("Art Style", "select", ["", "Abstract", "Impressionist", "Pop art", "Cubism", "Surrealism", "Contemporary", "Fantasy"]),
+}
+
+const getControlQueryString = () => {
+  let queryString = "";
+  console.log(Object.values(advancedControlAttributes));advancedControlAttributes
+  for (let control of Object.values(advancedControlAttributes)) {
+    console.log(control);
+    queryString = queryString + ", " + control.current;
+  }
+  return queryString
+}
+
 const state = {
   canvas: null,
   imageElement: null,
   captionText: null,
+  showAdvancedControls: false
 };
 
 canva.onReady(async (opts) => {
@@ -16,6 +74,7 @@ canva.onReady(async (opts) => {
 });
 
 canva.onControlsEvent(async (opts) => {
+  let mval = opts.message.message.value;
   if (opts.message.controlId === "startRemoteImageProcessingButton") {
       console.log("generate image button pressed");
       console.log(opts);
@@ -23,7 +82,7 @@ canva.onControlsEvent(async (opts) => {
       console.log("calling canva.remoteProcess with opts:");
       opts = {
         settings: JSON.stringify({
-            caption: state.captionText,
+            caption: state.captionText + " " + getControlQueryString(),
         }),
       };
       console.log(opts);
@@ -40,9 +99,25 @@ canva.onControlsEvent(async (opts) => {
       console.log(opts);
       console.log(state);
       state[opts.message.controlId] = opts.message.message.value;
+  } else if (opts.message.controlType === "slider") {
+     const advancedControl = advancedControlAttributes[opts.message.controlId];
+     const val = advancedControlAttributes[opts.message.controlId].values[mval]
+     advancedControl.control.value = opts.message.message.value; 
+     advancedControl.current = val;
+  } else if (opts.message.controlType === "checkbox") {
+    if (opts.message.controlId === "showAdvancedControlsButton") {
+      state.showAdvancedControls = !state.showAdvancedControls
+    } else {
+      const advancedControl = advancedControlAttributes[opts.message.controlId];
+      const val = advancedControl.values[advancedControl.control.checked ? 0 : 1]
+      advancedControl.current = val;
+      advancedControl.control.checked = !advancedControl.control.checked;
+    }
   }
+  console.log(getControlQueryString());
   //const result = await canva.remoteProcess();
   //console.log(result);
+  renderControls();
 });
 
 canva.onImageUpdate(async (opts) => {
@@ -54,8 +129,39 @@ canva.onSaveRequest(async () => {
     return await imageHelpers.fromCanvas("image/jpeg", canvas);
 });
 
+const createControlsFromAttributes = (attributes) => {
+  if (!state.showAdvancedControls) return [];
+  const advControls = canva.create("group", {
+    id: "AdvancedControls",
+    label: "Advanced Controls",
+    children: []
+  })
+  const controls = [advControls]
+  Object.keys(attributes).map(attr => {
+    if (attributes[attr].length > 1) {
+      control = canva.create("slider", {
+        id: attr,
+        label: attr,
+        value: 0,
+        min: 0,
+        max: attributes[attr].length - 1,
+        step: 1
+      });
+    } else {
+      control = canva.create("checkbox", {
+        id: attr,
+        label: attr,
+        disabled: false,
+        checked: false
+      });
+    }
+    advControls.children.push(control);
+  })
+  return controls;
+}
+
 function renderControls() {
-  const controls = [
+  const mainControls = [
     canva.create("text_input", {
         inputType: "text",
         id: "captionText",
@@ -69,7 +175,21 @@ function renderControls() {
       id: "startRemoteImageProcessingButton",
       label: "Generate Image",
     }),
+    canva.create("checkbox", {
+      id: "showAdvancedControlsButton",
+      label: "Show Advanced Controls",
+      disabled: false,
+      checked: state.showAdvancedControls
+    }),
   ];
+
+  const advancedControls = Object.values(advancedControlAttributes).map(
+    attr => attr.control
+  );
+
+  const controls = state.showAdvancedControls
+    ? mainControls.concat(advancedControls)
+    : mainControls
   canva.updateControlPanel(controls);
 }
 
